7e520cbe63b4fb9f977b982cd958606f88bad3e6
fischteichit@gmx.de <charley>
2021-05-15 15:20:35 +0200

    fixed db_path

--- web_server_mqqt_receiver/app/model.py
+++ web_server_mqqt_receiver/app/model.py
@@ -1,9 +1,11 @@
 
 from peewee import *
 from datetime import datetime
+from os import path as op
 
 
 db = SqliteDatabase(None)
+PROJECT_DIR = op.dirname(__file__)
 
 
 class Textdata(Model):
@@ -23,7 +25,8 @@ class Textdata(Model):
 
 
 def initialize_models():
-    db.init('erika_text_data.db')
+
+    db.init(op.join(PROJECT_DIR,'..','erika_text_data.db'))
     # Unique constraint will take care of MQQT qos level 1, which can send msgs more than once
     Textdata.add_index(Textdata.hashid, Textdata.content, unique=True)
     db.create_tables([Textdata])
