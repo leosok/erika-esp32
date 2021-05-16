2a67a31f8cc36312051f95f4dd23556a6266eecc
fischteichit@gmx.de <charley>
2021-05-16 03:32:02 +0200

    removed uniq

--- web_server_mqqt_receiver/app/model.py
+++ web_server_mqqt_receiver/app/model.py
@@ -43,5 +43,6 @@ def initialize_models():
 
     db.init(op.join(PROJECT_DIR,'..','erika_text_data.db'))
     # Unique constraint will take care of MQQT qos level 1, which can send msgs more than once
-    Textdata.add_index(Textdata.hashid, Textdata.content, unique=True)
+    # -> removed because blank lines would not save, but doubble entries did not occur
+    # Textdata.add_index(Textdata.hashid, Textdata.content, unique=True)
     db.create_tables([Textdata])
